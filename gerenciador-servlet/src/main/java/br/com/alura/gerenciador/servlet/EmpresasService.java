da4afc53c517ee986d74f25af8d13b6272510059
Matheus Ednei <ednei880@gmail.com>
2023-05-17 14:06:55 -0300

    adicionando tratamento para lidar com xml e json

--- gerenciador-servlet/src/main/java/br/com/alura/gerenciador/servlet/EmpresasService.java
+++ gerenciador-servlet/src/main/java/br/com/alura/gerenciador/servlet/EmpresasService.java
@@ -24,20 +24,29 @@ public class EmpresasService extends HttpServlet {
 
 		List<Empresa> empresas = new Banco().getEmpresas();
 		
-		XStream xstream = new XStream();
-		xstream.alias("empresa", Empresa.class);
-		String xml = xstream.toXML(empresas);
-	
-		response.setContentType("application/xml");
-		response.getWriter().print(xml);
+		String valor = request.getHeader("Accept");
 		
+		if (valor.contains("xml")) {
+			
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class);
+			String xml = xstream.toXML(empresas);
 		
-//		List<Empresa> empresas = new Banco().getEmpresas();
-//		Gson gson = new Gson();
-//		String json = gson.toJson(empresas);
-//		
-//		response.setContentType("application/json");
-//		response.getWriter().print(json);
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+			
+		} else if (valor.contains("json")) {
+			
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+			
+			response.setContentType("application/json");
+			response.getWriter().print(json);	
+			
+		} else {
+			response.setContentType("application/json");
+			response.getWriter().print("{'message': 'no content'}");
+		}
 
 	}
 
